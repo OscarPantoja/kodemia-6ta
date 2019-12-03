0e113b678568fa266b1080d1ec5f270328cfd85d
OscarPantoja <oskar.pantoja@gmail.com>
2019-12-02 22:05:44 -0600

    :octocat: divisas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 console.log('hello koders')
+
+var pesoMexicano = 100
+var dolarAmericano = pesoMexicano/19.57
+var euros = pesoMexicano/21.68
+var pesoCubanoIntercambiable = (pesoMexicano/.80)/25.74
