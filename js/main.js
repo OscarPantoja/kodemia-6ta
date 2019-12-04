5c800d093de136871df735858bf5ac604315a6d2
OscarPantoja <oskar.pantoja@gmail.com>
2019-12-03 18:41:38 -0600

    tarea1

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ var pesoMexicano = 100
 var dolarAmericano = pesoMexicano/19.57
 var euros = pesoMexicano/21.68
 var pesoCubanoIntercambiable = (pesoMexicano/.80)/25.74
+
+
+"Tengo 30 años 11 meses 1 dia ¿cuantos segundo he vivido?"
+
+var totalDias = ((30*365)+(11*30)+1)
+var totalHoras = totalDias * 24
+var totalMinutos = totalHoras * 60
+var totalSegundos = totalMinutos * 60
