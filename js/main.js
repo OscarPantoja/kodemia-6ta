d60d110420292526b06a4f5f0a1b3a5d66287bb9
OscarPantoja <oskar.pantoja@gmail.com>
2019-12-05 18:57:34 -0600

    rama clase 3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 console.log('hello koders')
+
+
+
+var word = 'perro'
+var vowels = 'aeiou'
+​
+var count = 0 
+var vowelsAmount = 0 
+while (count < word.length) {
+    let letter = word[count]
+    if (vowels.includes(letter)) {
+        vowelsAmount++
+    }
+    count++
+}
+​
+if (vowelsAmount >= 3) {
+    console.log(`En efecto brou tiene ${vowelsAmount} vocales`);
+} else {
+    console.warn('Te falta brou');
+}
+
+
+
+
