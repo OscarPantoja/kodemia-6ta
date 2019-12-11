2782fc9c05159c8467b25459e67e06d317b6c031
OscarPantoja <oskar.pantoja@gmail.com>
2019-12-10 22:00:17 -0600

    ejercicios en clase

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 console.log('hello koders')
+
+
+var words = ['ana','juan','pedro','Agua','Reto','uno','Erik']
+
+var vowels = 'aA'
+
+const filterWordsA = (word) => words.filter(word => vowels.includes(word[0])) 
+console.log(filterWordsA(words))
+
+
+var vowels = 'aeiouAEIOU'
+
+const filterWords = (word) => words.filter(word => vowels.includes(word[0])) 
+console.log(filterWords(words))
+
+
+
+
+
+var wordsB = ['ana','juan','pedro','Agua','Reto']
+
+const initialLetter = words => words.reduce ((a,b) => a + b[0] , '')
+console.log(initialLetter(words))
