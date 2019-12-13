d96277a19e076c13e5bf120afdff075cad19b58b
OscarPantoja <oskar.pantoja@gmail.com>
2019-12-12 22:11:46 -0600

    ejercicios en clase

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
-console.log('hello koders')
+//console.log('hello koders')
+
+var arreglo = [
+    ['Jorge', 'Perez', 23],
+    ['Juan', 'Torrez', 25],
+    ['Daniel', 'Guzman', 30]
+]
+
+function Persona(nombre, apellido, edad){
+    this.nombre = nombre
+    this.apellido = apellido
+    this.edad = edad
+}
+
+//var persona = new Persona ('Jore', 'Perez', 23)
+//var personaDos = new Persona ('Jge', 'Perez', 23)
+//var personaTres = new Persona ('Je', 'Perez', 23)
+
+//const personaMatrix = matrix 
+
+
+
+//const parsePersonMatrix = matrix => {
+//    return matrix.map(person => new Person(person[0], person[1], person[2])).map(({ firstName, lastName }) => {
+//      return `${firstName} ${lastName}`
+//    })
+//  }
+
+
+var numberOne = 2
+var numberTwo = 10
+
+for (var i = 0; i < numberTwo.lenght; i++ ) {
+    var multiplos = ; 
+}
+    console.log(multiplos)
+
+
+
+var array = [
+    {
+        'clave' : 'valor'
+        'clave2' : 'valor2'
+    }
+]
+    
+array.map(function(array)){
+    cvalue
+}
+
